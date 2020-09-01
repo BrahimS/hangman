4da3b698704d1912cdd90ff10c34f78fbe67e037
BS <bs@macbook-pro-de-bs.home>
2020-09-01 08:52:45 +0200

    add buttonP component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom'
 import './style.css'
 
 //import DashBoard from './components/Dashbord'
+
+class ButtonPad extends Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			letters    : [
+				['A', 'B', 'C', 'D', 'E', 'F'],
+				['G', 'H', 'I', 'K', 'L', 'M'],
+				['N', 'O', 'P', 'Q', 'R', 'S'],
+				['T', 'U', 'V', 'X', 'Y', 'Z']
+			],
+			targetWord : props.targetWord
+		}
+	}
+	render () {
+		const letterGrid = this.state.letters.map((row, index) => (
+			<p key={index}>{row.map((letter, index) => <button key={index}>{letter}</button>)}</p>
+		))
+
+		return <div>{letterGrid}</div>
+	}
+}
 class Letter extends Component {
 	render () {
 		return <span className="letter">{this.props.value}</span>
@@ -19,6 +41,9 @@ class DashBoard extends Component {
 			<div>
 				<h2>This is the dashboard</h2>
 				<div>{letters}</div>
+				<div>
+					<ButtonPad targetWord={this.props.targetWord} />
+				</div>
 			</div>
 		)
 	}
